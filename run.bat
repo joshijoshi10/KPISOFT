2f5463126c8b6a980a2338f1f90118a3208b1e57
santosh <joshijoshi10@gmail.com>
2021-05-27 07:16:52 +0530

    bat file modified

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-cd D:\cucumber videos\onedrive1\Desktop\New folder (2)\New folder\nightlytest\pom.xml
+cd D:\cucumber videos\onedrive1\Desktop\New folder (2)\New folder\nightlytest
 mvn clean install
